31f4844b28d08fd42f2250693d412a5cf3a545d9
Suzette Becraft <suzettebecraft@hotmail.com>
2026-02-15 04:31:14 -0500

    Add Homepage component, create /src/asset folder and move static map and svg map into assets

--- src/app/country-data/country-data.ts
+++ src/app/country-data/country-data.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-country-data',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './country-data.css',
 })
 export class CountryData {
-
+  @Input() country: any;
 }
