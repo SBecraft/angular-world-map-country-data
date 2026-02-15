15617eecea3c3cd5252fdf994c43fbf584282892
Suzette Becraft <suzettebecraft@hotmail.com>
2026-02-15 04:50:14 -0500

    Set routing in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomepageComponent
+  }
+];
+
+
